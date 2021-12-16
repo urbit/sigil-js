5624348dfed3609a91921faffad1171eaefcc095
Gavin <gavin@Gavins-MacBook-Pro.local>
2021-12-16 17:16:21 -0500

    fix:sun stroke and stroke-width

--- preview/pages/index.js
+++ preview/pages/index.js
@@ -1,13 +1,41 @@
 import Head from 'next/head'
 import { useState } from 'react'
+import ob from 'urbit-ob'
 import sigil from '../lib/index'
 
 const sequence = num => Array.from(Array(num), (_, i) => i);
 
+const compose = (...fs) => {
+  return fs.reduceRight((f, g) => (...args) => g(f(...args)), (v) => v)
+}
+const randInt = max => Math.floor(Math.random() * Math.floor(max));
+
+const noSig = str => str.replace(/~+/g, '');
+
+const COMET = 'COMET'
+const MOON = 'MOON'
+const PLANET = 'PLANET'
+const STAR = 'STAR'
+const GALAXY = 'GALAXY'
+
+
+const randPatp = k => {
+  let b = 8;
+  if (k === COMET) b = 128;
+  if (k === MOON) b = 64;
+  if (k === PLANET) b = 32;
+  if (k === STAR) b = 16;
+  if (k === GALAXY) b = 8;
+  return randInt(Math.pow(2, b) - 1);
+}
+
+
+const randPlanet = () => compose(noSig, ob.patp, randPatp)(PLANET);
+
 export default function Home() {
   // console.log(sigil)
 
-  const [point, setPoint] = useState('zod')
+  const [point, setPoint] = useState('ridlur-figsun')
   const [size, setSize] = useState(150)
   const [qty, setQty] = useState(1)
   const [detail, setDetail] = useState('default')
@@ -30,8 +58,8 @@ export default function Home() {
                     point: point, 
                     size:size,
                     padding:0,
-                    background:'white', 
-                    foreground:'black',
+                    background:'#010101', 
+                    foreground:'yellow',
                     detail:detail,
                     space:space,
                   }) 
@@ -46,6 +74,13 @@ export default function Home() {
           className="hidden md:flex absolute top-0 left-0 m-4 flex-col">
           <div className="bg-white p-4 rounded-2xl flex flex-col">
             <h4 className="text-lg font-bold">Sigil-js V2.0</h4>
+
+            <button
+              className="mt-2"
+              onClick={() => setPoint(randPlanet())}
+            >
+              Random
+            </button>
 
             <button
               className="mt-2"
@@ -113,6 +148,18 @@ export default function Home() {
 
             <hr className="mt-4 mb-2 p-0 -mx-4" />
             <b>Size</b>
+            <button
+              className="mt-2"
+              onClick={() => setSize(500)}
+            >
+              500
+            </button>
+            <button
+              className="mt-2"
+              onClick={() => setSize(100)}
+            >
+              100
+            </button>
             <button
               className="mt-2"
               onClick={() => setSize(32)}
